a58a7058a621cfe5300422c17cf87c2ed9fd9373
Alejandra SM <alejandrasanmartin2911@gmail.com>
2023-09-28 16:02:08 -0500

    Initial Commit

---
+++ Practice/PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeApp.swift
+//  Practice
+//
+//  Created by Alejandra San Martin on 28/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
